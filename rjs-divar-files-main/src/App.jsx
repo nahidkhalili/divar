c9d9acf68290200365035734705a264c8445084d
Nahid Khalili <Nahid_khalili@ymail.com>
2025-03-04 23:26:29 +0330

    feat: add defaultOptions to react query

--- rjs-divar-files-main/src/App.jsx
+++ rjs-divar-files-main/src/App.jsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/Router";
 import { BrowserRouter } from "react-router-dom";
+import defaultOptions from "./config/reactQuery";
+
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({defaultOptions:defaultOptions});
   return (
     <>
       <BrowserRouter>
