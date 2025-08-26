4f4ff031b0221afc956ebcd6c9a4d35bbb3a5f75
Nahid Khalili <Nahid_khalili@ymail.com>
2025-08-26 13:37:06 +0330

    remove old folder

--- rjs-divar-files-main/src/layouts/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-center ltr mt-[100px] pt-[10px] border-t-2 border-solid border-[rgb(234,234,234)]">
-      <div>Developed by Nahid with ❤</div>
-    </footer>
-  );
-};
-
-export default Footer;
