bccf1549b1db2290eb22c60ea32cc0fcaffa8084
Nahid Khalili <Nahid_khalili@ymail.com>
2025-08-26 17:51:27 +0330

    feat:a dropdown added for divare-man link

--- divar/src/components/modules/DropDownMenu.jsx
+++ divar/src/components/modules/DropDownMenu.jsx
@@ -1,15 +1,80 @@
-const DropDownMenu = () => {
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import { useGetProfile } from "../../services/queries";
+import { e2p } from "../../utils/number";
+
+const DropDownMenu = ({ className = "", closeMenu }) => {
+  const { data, isLoading, isError } = useGetProfile();
+  console.log("isloading", isLoading, isError);
   return (
-    <div className="absolute w-[250px] border-2 top-12 right-0">
-      <ul>
-        <li className="flex flex-row">
-          <img src="/log.png" />
-          <span>ورود</span>
-        </li>
-        <li>
-          
-        </li>
-      </ul>
+    <div
+      className={`px-4 py-3  bg-white w-[220px] rounded-[5px] shadow-[0_0_10px_rgba(0,0,0,0.15)] text-gray-500 text-[13px] ${className}`}
+    >
+      <div>
+        {data ? (
+          <Link to="/dashboard">
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2" onClick={closeMenu}>
+              <img src="/profile.svg" className="w-[15px]" />
+              <div className="flex flex-col justify-start gap-1">
+                <span>کاربر دیوار</span>
+                <span className="text-gray-300">{e2p(data.data.mobile)}</span>
+              </div>
+            </div>
+          </Link>
+        ) : (
+          <div className="flex flex-row cursor-pointer py-0 justify-start items-baseline gap-2">
+            <img src="/profile.svg" className="w-[15px]" />
+            <Link to="/auth">
+              <span>ورود</span>
+            </Link>
+            <hr className="border-gray-300" />
+          </div>
+        )}
+
+        <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+          <img src="/profile.svg" className="w-[15px]" />
+          <span>دیوار حرفه ای</span>
+        </div>
+        {data && (
+          <>
+            <hr className="border-gray-300" />
+
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>تایید هویت</span>
+            </div>
+            <hr className="border-gray-300" />
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>آگهی های من</span>
+            </div>
+            <hr className="border-gray-300" />
+
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>پرداخت های من</span>
+            </div>
+            <hr className="border-gray-300" />
+
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>نشان ها</span>
+            </div>
+            <hr className="border-t border-gray-300" />
+
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>بازدیدهای اخیر</span>
+            </div>
+            <hr className="border-gray-300" />
+
+            <div className="flex flex-row cursor-pointer py-3 justify-start items-baseline gap-2">
+              <img src="/profile.svg" className="w-[15px]" />
+              <span>خروج</span>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- divar/src/layouts/Header.jsx
+++ divar/src/layouts/Header.jsx
@@ -1,25 +1,67 @@
 import { Link } from "react-router-dom";
 import DropDownMenu from "../components/modules/DropDownMenu";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const [activeHeader, setActiveHeader] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const closeMenu = () => {
+    setActiveHeader(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        closeMenu();
+      }
+    };
+
+    if (activeHeader) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [activeHeader]);
+
   return (
     <header className="flex flex-row justify-between items-center border-b-2 border-solid border-[rgb(234,234,234)] py-[10px] px-0 mb-[20px]">
       <div className="flex flex-row gap-8">
         <Link to="/">
-          <img className="w-[45px] ml-[40px]" src="divar.svg" />
+          <img className="w-[45px] ml-[40px]" src="/divar.svg" />
         </Link>
         <span className="flex flex-row items-center text-gray-400 h-[50px]">
-          <img className="w-[20px]" src="location.svg" />
+          <img className="w-[20px]" src="/location.svg" />
           <p className="text-[0.9rem] mr-2">تهران</p>
         </span>
       </div>
       <div className="flex flex-row items-center gap-8">
-        <Link to="/auth">
-          <span className="relative flex flex-row items-center px-6 text-gray-400 h-[50px]">
-            <img className="w-[20px]" src="profile.svg" />
-            <p className="text-[0.9rem] mr-2">دیوار من</p>
-              <DropDownMenu />
-          </span>
+        <div className="relative" ref={dropdownRef}>
+          <div
+            className=" flex flex-row items-center cursor-pointer w-[140px] px-6 text-gray-500 h-[50px] rounded-[3px] hover:bg-gray-200 active:bg-gray-200"
+            onClick={() => setActiveHeader(!activeHeader)}
+          >
+            <img className="w-[14px]" src="/profile.svg" />
+            <p className="text-[0.8rem] mr-2">دیوار من</p>
+          </div>
+          {activeHeader && (
+            <DropDownMenu className="absolute top-12" closeMenu={closeMenu} />
+          )}
+        </div>
+
+        <Link to="/support">
+          <div className=" flex flex-row cursor-pointer items-center  w-[140px] px-6 text-gray-500 h-[50px] rounded-[3px] hover:bg-gray-200  active:bg-gray-200">
+            <img className="w-[18px]" src="/message-bubble.png" />
+            <p className="text-[0.8rem] mr-2"> چت و تماس</p>
+          </div>
+        </Link>
+        <Link>
+          <div className=" flex flex-row cursor-pointer w-[140px] items-center px-6 text-gray-500 h-[50px] rounded-[3px] hover:bg-gray-200  active:bg-gray-200">
+            <img className="w-[18px]" src="/buoy.png" />
+            <p className="text-[0.8rem] mr-2"> پشتیبانی</p>
+          </div>
         </Link>
         <Link
           className="bg-[var(--red-color)] text-white h-[40px] w-[80px] leading-[40px] text-center rounded-md mr-[40px]"
@@ -27,7 +69,6 @@ const Header = () => {
         >
           ثبت آگهی
         </Link>
-      
       </div>
     </header>
   );
